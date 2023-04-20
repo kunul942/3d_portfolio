a0e006f187e32cd0346b752d3b5db9f5d2b63ae9
kunul942 <jhonatanlee942@gmail.com>
2023-04-19 18:00:46 -0600

    Fix problems

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -23,11 +23,12 @@ const Computers = ({ isMobile }) => {
                 castShadow
                 shadow-mapSize={1024}
             />
-
+ 
             {/* dimensiones y medidas */ }
             <primitive 
                 object={ computer.scene } 
-                scale={ isMobile ? 0.7 : 0.75}
+                // scale={ isMobile ? 0.7 : 0.75}
+                scale={ isMobile ? 0.5 : 0.55}
                 position={ isMobile ? [0, -3, -2.2] : [0, -3.25,-1.5]}
                 rotation={[-0.01, -0.2, -0.1]}
             />
