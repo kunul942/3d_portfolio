a39b9ee2753a0827650a1243a057cbaa4447309b
kunul942 <jhonatanlee942@gmail.com>
2023-04-19 22:52:21 -0600

    fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,70 +1,71 @@
-import { Suspense, useEffect, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+// import { Suspense, useEffect, useState } from 'react'
+// import { Canvas } from '@react-three/fiber'
+// import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
-import CanvasLoader from '../Loader'
+// import CanvasLoader from '../Loader'
 
-const Computers = ({ isMobile }) => {
-
-    //computadora 3D
-    const computer = useGLTF('./desktop_pc/scene.gltf')
+// const Computers = ({ isMobile }) => {
 
 
-    return (
+//computadora 3D
+//     const computer = useGLTF('./desktop_pc/scene.gltf')
 
 
-        <mesh>
-            {/* luces para el pc*/ }
-            <hemisphereLight intensity={0.15} groundColor="black" />
-            <pointLight intensity={1} />
-            <spotLight 
-                position={[-20, 50, 10]} 
-                angle={0.12}
-                penumbra={1}
-                intensity={1}
-                castShadow
-                shadow-mapSize={1024}
-            />
+//     return (
+
+
+//         <mesh>
+//             {/* luces para el pc*/ }
+//             <hemisphereLight intensity={0.15} groundColor="black" />
+//             <pointLight intensity={1} />
+//             <spotLight 
+//                 position={[-20, 50, 10]} 
+//                 angle={0.12}
+//                 penumbra={1}
+//                 intensity={1}
+//                 castShadow
+//                 shadow-mapSize={1024}
+//             />
  
-            {/* dimensiones y medidas */ }
-            {/* <primitive 
-                object={ computer.scene } 
-                scale={ isMobile ? 0.7 : 0.75}
-                position={ isMobile ? [0, -3, -2.2] : [0, -3.25,-1.5]}
-                rotation={[-0.01, -0.2, -0.1]}
-            /> */}
-            <primitive 
-                object={ computer.scene } 
-                scale={ 0.60 }
-                position={ [0, -3.25,-1.5]}
-                rotation={[-0.01, -0.2, -0.1]}
-            />
-        </mesh>
-    )
-}
+//             {/* dimensiones y medidas */ }
+//             {/* <primitive 
+//                 object={ computer.scene } 
+//                 scale={ isMobile ? 0.7 : 0.75}
+//                 position={ isMobile ? [0, -3, -2.2] : [0, -3.25,-1.5]}
+//                 rotation={[-0.01, -0.2, -0.1]}
+//             /> */}
+//             <primitive 
+//                 object={ computer.scene } 
+//                 scale={ 0.60 }
+//                 position={ [0, -3.25,-1.5]}
+//                 rotation={[-0.01, -0.2, -0.1]}
+//             />
+//         </mesh>
+//     )
+// }
 
 
 
-const ComputerCanvas = () => {
+// const ComputerCanvas = () => {
 
     //mobile
     // const [isMobile, setIsMobile] = useState( false )
     // useEffect(() => {
-    //     // Add a listener for changes to the screen size
+        // Add a listener for changes to the screen size
     //     const mediaQuery = window.matchMedia("(max-width: 500px)")
     //     console.log({ mediaQuery: mediaQuery.matches })
-    //     //Set the initial value of the 'isMobile state variable'
+        //Set the initial value of the 'isMobile state variable'
     //     setIsMobile( mediaQuery.matches )
 
-    //     //Define a callback function to handle changes to the media query
+        //Define a callback function to handle changes to the media query
     //     const handleMediaQueryChange = ( event ) =>{
     //         setIsMobile( event.matches )
     //     }  
 
-    //     //Add the callback function as a listener for changes to the media query
+        //Add the callback function as a listener for changes to the media query
     //     mediaQuery.addEventListener("change", handleMediaQueryChange) 
 
-    //     //Remove the listener when the component is unmounted
+        //Remove the listener when the component is unmounted
     //     return () =>{ 
     //         mediaQuery.removeEventListener("change", handleMediaQueryChange)
     //     }
@@ -72,36 +73,93 @@ const ComputerCanvas = () => {
     // }, [])
     
 
+//     return(
+//         <Canvas 
+//             frameloop="demand" 
+//             shadows 
+//             gl = {{ preserveDrawingBuffer: true }}
+//             camera= {{ 
+//                 fov: 25,  
+//                 near: 0.1,
+//                 far: 200,
+//                 position: [ 20, 3, 5 ], 
+//             }}
+//         >
+//             <Suspense fallback={ <CanvasLoader /> }>
+//                 <OrbitControls 
+//                     autoRotate
+//                     enableZoom={ false } 
+//                     maxPolarAngle={Math.PI / 2}
+//                     minPolarAngle={Math.PI / 2}
+//                 />
+//                 {/* <Computers isMobile={ isMobile } /> */}
+//                 <Computers />
+//             </Suspense>
+
+//             {/* <Preload all /> */}
+//         </Canvas>
+//     )
+// }
+
+
+
+
+
+// export default ComputerCanvas
+
+
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from '../Loader'
+
+
+const Computers = () => {
+
+    const earth = useGLTF('./planet/scene.gltf')
+
+
+    return (
+        <primitive 
+            object={ earth.scene }
+            scale={ 3 }
+            position={ [0, -3.25,-1.5]}
+            rotation={[-0.01, -0.2, -0.1]}
+        />
+    )
+}
+
+
+const ComputerCanvas = () =>{
+
     return(
-        <Canvas 
-            frameloop="demand" 
-            shadows 
-            gl = {{ preserveDrawingBuffer: true }}
-            camera= {{ 
-                fov: 25,  
+
+        <Canvas
+        frameloop="demand"
+            shadows
+            gl={{ preserveDrawingBuffer: true }}
+            camera={{ 
+                fov: 45,
                 near: 0.1,
                 far: 200,
-                position: [ 20, 3, 5 ], 
+                position: [-4, 3, 6]
             }}
         >
             <Suspense fallback={ <CanvasLoader /> }>
                 <OrbitControls 
                     autoRotate
-                    enableZoom={ false } 
-                    maxPolarAngle={Math.PI / 2}
-                    minPolarAngle={Math.PI / 2}
+                    enableZoom={ false }
+                    maxPolarAngle={ Math.PI / 2 }
+                    minPolarAngle={ Math.PI / 2 }
                 />
-                {/* <Computers isMobile={ isMobile } /> */}
+
                 <Computers />
             </Suspense>
-
-            {/* <Preload all /> */}
         </Canvas>
     )
 }
-
-
-
 
 
 export default ComputerCanvas
