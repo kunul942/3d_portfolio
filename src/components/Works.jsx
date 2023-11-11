2f0e649d9cc90549623644c32e9584f167ea054e
kunul942 <jhonatanlee942@gmail.com>
2023-11-10 20:14:54 -0600

    done

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -14,7 +14,7 @@ const ProjectCard =({ index, name, description, tags, image, source_code_link, u
         <motion.div variants={ fadeIn("up", "spring", index * 0.5, 0.75) }>
             <Tilt 
                 options={{max: 45, scale: 1, speed: 450}}
-                className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+                className="bg-tertiary p-6 rounded-2xl sm:w-[360px] w-full"
             >   
                 {/* mostrar card de los proyectos */}
                 <div className="relative w-full h-[230px]">
