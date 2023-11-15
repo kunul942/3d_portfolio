9cbb091281c8ae0eb867adfe721b030de6fabb4f
kunul942 <jhonatanlee942@gmail.com>
2023-11-15 00:18:49 -0600

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () =>{
                         Hi, I'm <span className="text-[#915eff]">Kunu</span>
                     </h1>
                     <p className={`${ styles.heroSubText } mt-2 text-white-100`}>
-                        I Develop Full Stack Web Applications <br className="sm:block hidden"/> 
+                        I develop Full Stack Web Applications <br className="sm:block hidden"/> 
                     </p>
                 </div>
             </div>
